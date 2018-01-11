1512e7ae6a42e35bcafd64eefc0ad0ce067f24e2
Hansraj Das <raj.das.136@gmail.com>
2018-01-11 21:32:28 +0530

    Level order traversal - O(n) solution using queue

--- level_order_tree_traversal_using_queue.c
+++ level_order_tree_traversal_using_queue.c
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 
 /*
- * Date: 2017-01-07
+ * Date: 2017-01-11
  *
  * Implementation: Uses DFS graph traversal approach.
  * 1. Prints current node data.
@@ -10,8 +10,11 @@
  * 3. dequeue a node from queue.
  * 4. Execute while queue is not empty.
  *
- * Complexity: O(n), n is number of node in tree.
+ * Complexity: O(n), n is number of node in tree. Requires extra space to
+ * maintain queue.
  */
+
+#define MAX_Q_SIZE 100
 
 typedef struct node {
   int data;
@@ -27,9 +30,52 @@ node* new_node(int d) {
   return new_node;
 }
 
+// Create and initialze an empty Q.
+node** createQ(int *front, int *rear) {
+  int idx = 0;
+  node **Q = (node **)malloc(sizeof(node) * MAX_Q_SIZE);
+
+  for (idx = 0; idx < MAX_Q_SIZE; idx++) {
+    Q[idx] = NULL;
+  }
+  *front = 0;
+  *rear = 0;
+  return Q;
+}
+
+// Insert item at rear.
+void enQ(node **Q, int *rear, node *current_node) {
+  Q[*rear] = current_node;
+  (*rear)++;
+}
+
+// Return item at front from the Q.
+node* deQ(node **Q, int *front) {
+  (*front)++;
+  return Q[*front - 1];
+}
+
 // Implements a queue to perform level order traversal of tree.
 void print_level_order(node *root) {
+  int front = 0, rear = 0;
+  node *temp_node = root;
+  node **tQ = createQ(&front, &rear);
+
   printf("********* Level order traversal *********\n");
+  while (temp_node) {
+    printf("%d ", temp_node->data);
+
+    // Check and enQ left/right child of current node to Q.
+    if (temp_node->left) {
+      enQ(tQ, &rear, temp_node->left);
+    }
+    if (temp_node->right) {
+      enQ(tQ, &rear, temp_node->right);
+    }
+
+    // deQ next item from Q.
+    temp_node = deQ(tQ, &front);
+  }
   printf("\n");
 }
 
@@ -50,5 +96,5 @@ int main() {
 /*
  * Output:
  * ********* Level order traversal *********
- * 1 2 3 4 5 6 
+ * 1 2 3 4 5 6
  */
