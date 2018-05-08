218543a99991ca189253ce6a5f0952d70db63e52
Hansraj Das <raj.das.136@gmail.com>
2018-05-08 20:55:26 +0530

    Edit distance algo added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Algorithms in C and Python
 
 ## List of Algorithms
-- [Rabin Karp](rabin_karp.c): Hashing based substring matching algo.
+- [Rabin karp](rabin_karp.c): Hashing based substring matching algo.
 - [Karatsuba algo](karatsuba.py): Efficient way to multiply 2 numbers, karatsuba algo.
 - [Check prime](is_prime.go): Check if a number is prime or not.
 - [Babylonian - Square root](babylonian_square_root.go): Babylonian method to find square root of a number.
@@ -13,4 +13,5 @@
 - [Level order tree traversal using queue](level_order_tree_traversal_using_queue.c): Level order traversal of a tree using queue | O(n) solution, requires extra space to manage queue.
 - [Shortest path of DAG](shortest_path_for_DAG.py): Find shortest in a directed acyclic graph from a source vetrex to all reachable vertex | O(V + E).
 - [Dijkstra algo](dijkstra.py): Pending :(
-- [Bellman Ford](bellman_ford.py): Bellman ford algo to find shortest path in a graph | O(VE).
+- [Bellman ford](bellman_ford.py): Bellman ford algo to find shortest path in a graph | O(VE).
+- [Edit distance](edit_distance.c): Find minimum operations required to convert a source string to target string.
---
+++ edit_distance.c
@@ -0,0 +1,88 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+/*
+ * Date: 2018-05-07
+ *
+ * Description:
+ *  Find operations required to change a source string(str1) to target(str2).
+ *  Operations allowed are (on source string):
+ *    Insert a character 
+ *    Remove a character or
+ *    Replace a character
+ *
+ * Complexity:
+ *  Time: O(mxn)
+ *  Space: O(mxn)
+ */
+
+int min(int a, int b, int c) {
+  return a < b ? (a < c ? a : c): (b < c ? b : c);
+}
+
+int edit_distance(char s1[], char s2[], int m, int n) {
+  int i = 0, j = 0;
+
+  /* Store results of subproblems. */
+  int **dp = (int **)malloc((m + 1)*sizeof(int *));
+
+  for (i = 0; i <= m; i++)
+    dp[i] = (int *)malloc(sizeof(n + 1)*sizeof(int));
+
+  /* Fill dp in bottom up manner. */
+  for (i = 0; i <= m; i++) {
+    for (j = 0; j <= n; j++) {
+      /*
+       * If first string is empty, insert all characters present in second
+       * string.
+       */
+      if (i == 0)
+        dp[i][j] = j;
+      /*
+       * If second string is empty, remove all characters present in first
+       * string.
+       */
+      else if (j == 0)
+        dp[i][j] = i;
+      /*
+       * If current characters are same in both strings, nothing to do, just
+       * copy the operations required till previous string lengths.
+       */
+      else if (s1[i - 1] == s2[j - 1])
+        dp[i][j] = dp[i - 1][j - 1];
+      /*
+       * If current characters are different, check how minimum can be achieved
+       * using inserting, removing or deleting a character.
+       */
+      else
+        dp[i][j] = 1 + min(
+            dp[i][j - 1],  // Insert
+            dp[i - 1][j],  // Remove
+            dp[i - 1][j - 1]);  // Replace
+    }
+  }
+  return dp[m][n];
+}
+
+int main() {
+  char str1[50] = {'\0'};
+  char str2[50] = {'\0'};
+
+  printf("Enter first string: ");
+  scanf("%s", str1);
+  
+  printf("Enter second string: ");
+  scanf("%s", str2);
+
+  printf("Operations required to convert from %s to %s: %d\n",
+    str1, str2, edit_distance(str1, str2, strlen(str1), strlen(str2)));
+ }
+
+
+/*
+ * Output:
+ *  Enter first string: sunday
+ *  Enter second string: saturday
+ *  Operations required to convert from sunday to saturday: 3
+ */
