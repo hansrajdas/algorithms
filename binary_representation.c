6d4fa62b5c2da91611b1d5c25ec5122a7fe7abdd
Hansraj Das <raj.das.136@gmail.com>
2018-07-03 22:15:32 +0530

    Space added between each nibble

--- binary_representation.c
+++ binary_representation.c
@@ -12,12 +12,20 @@
 
 void binary_representation(int n) {
   unsigned short int size = sizeof(int) * 8;
+  unsigned short int space = 0;
   unsigned int i = 0;
   printf("Binary representation of %d is: ", n);
 
   // Checking bit at individual position and printing 0 or 1.
-  for (i = 1 << size - 1; i > 0; i = i/2)
-    (n & i) ? printf("1") : printf("0");
+  for (i = 1 << size - 1; i > 0; i = i/2) {
+    if (space == 4) {
+      space = 0;
+      (n & i) ? printf(" 1") : printf(" 0");
+    } else {
+      (n & i) ? printf("1") : printf("0");
+    }
+    space++;
+  }
 }
 
 int main() {
@@ -28,3 +36,23 @@ int main() {
   printf("\n");
   return 0;
 }
+
+/*
+Enter number: 2
+Binary representation of 2 is: 0000 0000 0000 0000 0000 0000 0000 0010
+
+Enter number: 5
+Binary representation of 5 is: 0000 0000 0000 0000 0000 0000 0000 0101
+
+Enter number: 1
+Binary representation of 1 is: 0000 0000 0000 0000 0000 0000 0000 0001
+
+Enter number: -1
+Binary representation of -1 is: 1111 1111 1111 1111 1111 1111 1111 1111
+
+Enter number: 44
+Binary representation of 44 is: 0000 0000 0000 0000 0000 0000 0010 1100
+
+Enter number: 128
+Binary representation of 128 is: 0000 0000 0000 0000 0000 0000 1000 0000
+*/
