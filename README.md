f9ae028d4fd3f4745ffc43b6349c1ea909fd09b5
Hansraj Das <raj.das.136@gmail.com>
2018-08-02 22:23:34 +0530

    Tower of hanoi

--- README.md
+++ README.md
@@ -74,5 +74,6 @@
 - [Generate all subsets, binary method**](generate_all_subsets_binary_method.py): Generate all subsets of a given set, binary representation method | O(n2^n) | Level 2.
 - [Multiply 2 integers](multiply_integers.c): Multiply 2 positive integers without using multiply operator | O(log(s)) | Level 3.
 - [Print fibonacci numbers](fibonacci.c): Print first n fibonacci numbers | O(n) | Level 1.
+- [Tower of hanoi](tower_of_hanoi.c): Print steps to solve tower of hanoi problem for n disks | O(2^n) | Level 3.
 - [PENDING](cut_rod_to_max_profit.c): https://www.geeksforgeeks.org/dynamic-programming-set-13-cutting-a-rod/ | O(n^2) | Level 3.
 - [PENDING](cut_sequence_for_min_cost.c): 1st problem from https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/recitation-videos/MIT6_006F11_rec24.pdf | O(n^3) | Level 3.
---
+++ tower_of_hanoi.c
@@ -0,0 +1,73 @@
+/*
+ * Date: 2018-08-02
+ *
+ * Description:
+ * Tower of Hanoi is a mathematical puzzle where we have three rods and n disks.
+ * The objective of the puzzle is to move the entire stack to another rod,
+ * obeying the following simple rules:
+ * 1) Only one disk can be moved at a time.
+ * 2) Each move consists of taking the upper disk from one of the stacks and
+ *    placing it on top of another stack i.e. a disk can only be moved if it is
+ *    the uppermost disk on a stack.
+ * 3) No disk may be placed on top of a smaller disk.
+ *
+ * Approach:
+ * We can try solving this problem with n = 1, 2, 3 and follow the pattern
+ * recursively.
+ * Calling 3 towers with character - A, B and C.
+ *
+ * Complexity:
+ * O(2^n), For n disks 2^n - 1 steps are required.
+ */
+
+#include "stdio.h"
+
+/*
+ * Prints steps to solve TOH problem, moves disks from srcTower to dstTower.
+ *
+ * Args:
+ * n: Number of disks.
+ * srcTower: Tower which contains all disks.
+ * dstTower: Tower where all disks needs to be moved.
+ * bufferTower: Buffer tower.
+ */
+void towerOfHanoi(int n, char srcTower, char dstTower, char bufferTower) {
+  if (n <= 0) return;
+  if (n == 1) {
+    printf("Move disk 1 from tower %c to %c\n", srcTower, dstTower);
+    return;
+  }
+  towerOfHanoi(n - 1, srcTower, bufferTower, dstTower);
+  printf("Move dist %d from tower %c to %c\n", n, srcTower, dstTower);
+  towerOfHanoi(n - 1, bufferTower, dstTower, srcTower);
+}
+
+int main() {
+  int i = 0;
+  int n = 0;
+  printf("Enter number of disks: ");
+  scanf("%d", &n);
+  towerOfHanoi(n, 'A', 'B', 'C');
+  return 0;
+}
+
+/*
+Output:
+************************
+Enter number of disks: 2
+Move disk 1 from tower A to C
+Move dist 2 from tower A to B
+Move disk 1 from tower C to B
+************************
+Enter number of disks: 3
+Move disk 1 from tower A to B
+Move dist 2 from tower A to C
+Move disk 1 from tower B to C
+Move dist 3 from tower A to B
+Move disk 1 from tower C to A
+Move dist 2 from tower C to B
+Move disk 1 from tower A to B
+************************
+Enter number of disks: 1
+Move disk 1 from tower A to B
+*/
