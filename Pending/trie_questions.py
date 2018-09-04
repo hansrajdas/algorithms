fc37ddd9ed57d655f39417581014d28593d91dbf
Hansraj Das <raj.das.136@gmail.com>
2018-09-04 23:30:08 +0530

    Trie problems, added in pendings/

---
+++ Pending/trie_questions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Date: 2018-
+#
+# Description:
+# 1. Insert and search in trie: https://www.geeksforgeeks.org/trie-insert-and-search/
+# 2. Delete from a trie: https://www.geeksforgeeks.org/trie-delete/
+# 3. Pattern matching using trie suffixes: https://www.geeksforgeeks.org/pattern-searching-using-trie-suffixes/
+#
+# Approach:
+#
+# Complexity:
+#
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
