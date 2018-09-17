368c04a17b98a40b5226dd24764b3f3c84239d0b
Hansraj Das <hansrajd@rodete-desktop-imager.corp.google.com>
2018-09-17 11:10:12 +0530

    vertical order tree traversal

---
+++ Pending/vertical_order_tree_traversal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Date: 2018-
+#
+# Description:
+# WAP to print binary tree in vertical level order. If graph is:
+#              5
+#             / \
+#            4   6
+#
+# It should print 4, 5, 6
+#
+# Approach:
+#
+# Complexity:
+#
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+  main()
