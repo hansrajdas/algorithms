3f80c974e1242775988a61986f9f9bcb93371b16
Hansraj Das <hansrajd@rodete-desktop-imager.corp.google.com>
2018-09-17 13:07:11 +0530

    BFS of graph

--- Level-3/graph_breadth_first_search.py
+++ Level-3/graph_breadth_first_search.py
@@ -3,16 +3,19 @@
 # Date: 2017-12-25
 #
 # Description:
-# Program creates a graph, adds few nodes to it and performs
-# breadth first traversal of graph. Uses dictionary to store adjacency list of
-# graph having list of adjacent nodes corresponding to each node.
+# Program creates a directed graph, adds few nodes to it and performs breadth
+# first traversal(BFS) of graph.
 #
-# Complexity: 
+# Approach:
+# Use a queue to store adjacent nodes, loop until queue becomes empty and keep
+# on adding adjacent nodes to queue while traversing.
+#
+# Complexity:
 # O(V + E)
 # V = number of vertexes/nodes
 # E = number of edges
 
-from collections import defaultdict
+import collections
 
 
 class Graph:
@@ -24,17 +27,19 @@ class Graph:
 
     Example: {0:[1, 2], 1:[0], 2: [0, 3]...}
     """
-
-    self.graph = defaultdict(list)
+    self.graph = collections.defaultdict(list)
 
   def add_edge(self, start, end):
     """
     Adds an edge to the adjacency list depending on start and end values.
-    if start is a new value inserted in graph, it adds a key to dictionary
+    If start is a new value inserted in graph, it adds a key to dictionary
     otherwise it updates existing node's adjacency list.
     Adds directed edge to graph.
-    """
 
+    Args:
+      start: Start node of edge.
+      end: End node of edge.
+    """
     self.graph[start].append(end)
 
   def bfs(self, start_node):
@@ -42,6 +47,9 @@ class Graph:
     Prints node values in breadth first search pattern.
     Initially marks all nodes as not visited(marked false) then starts with
     current node, traverses its adjacency list and goes in the same fashion.
+
+    Args:
+      start_ndoe: Start node for BFS traversal.
     """
 
     visited = {z: False for z in self.graph.keys()}
@@ -51,19 +59,18 @@ class Graph:
     while queue:
       current_node = queue.pop(0)
       print current_node
-      
+
       visited[current_node] = True
 
       for node in self.graph[current_node]:
         if visited.has_key(node):
-          if visited[node] == False:
+          if not visited[node]:
             queue.append(node)
             visited[node] = True
           else:
-            pass
+            pass  # Node already visited.
         else:
-          print "{0} - don't have any adjacent node".format(node)
-          visited[node] = True
+          print '{0} - do not have any adjacent node'.format(node)
 
 
 g = Graph()
@@ -77,3 +84,12 @@ g.add_edge(13, 12)
 g.add_edge(13, 14)
 
 g.bfs(10)
+
+
+# Output:
+# -------
+# 10
+# 11
+# 12
+# 13
+# 14 - do not have any adjacent node
