78e39f97b3dfc1ff23c3eb4e5de99f3ea0287b49
Hansraj Das <raj.das.136@gmail.com>
2018-09-18 22:29:15 +0530

    Example added in description

--- Level-2/sum_of_linked_lists_reverse_order.py
+++ Level-2/sum_of_linked_lists_reverse_order.py
@@ -6,7 +6,10 @@
 # Two numbers represented by a linked list, where each node contains a single
 # digit. The digits are stored in reverse order, such that the 1's digit is at
 # the head of the list. Write a function that adds the two numbers and returns
-# the sum as a linked list.
+# the sum as a linked list. Example:
+#
+# Input: (7 -> 1 -> 6) + (5 -> 9 -> 2). That is, 617 + 295
+# Output: 2 -> 1 -> 9. That is, 912
 #
 # Approach:
 # As numbers are stored in reverse order we can scan the two linked lists and
