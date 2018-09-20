a9080cda818ca3ebca1188e44e064b504136f1fd
Hansraj Das <hansrajd@rodete-desktop-imager.corp.google.com>
2018-09-20 13:35:12 +0530

    lowest common ancestor

---
+++ Pending/lowest_common_ancestor_in_binary_tree.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# Date: 2018-
+#
+# Description:
+# Find lowest common ancestor of given 2 nodes in binary tree.
+#
+# Approach:
+#
+# Complexity:
+#
+
+def main():
+  a = []
+  n = input('Enter number of elements: ')
+  for i in xrange (n):
+      x = input('Enter value of a[%d]: ' % i)
+      a.append(x)
+
+
+if __name__ == '__main__':
+  main()
