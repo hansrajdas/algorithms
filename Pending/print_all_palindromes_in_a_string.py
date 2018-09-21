cd6ddde8c597d5c222e57a99313f15b945386595
Hansraj Das <hansrajd@rodete-desktop-imager.corp.google.com>
2018-09-21 13:34:34 +0530

    string palindrome problems added

---
+++ Pending/print_all_palindromes_in_a_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# Date: 2018-
+#
+# Description:
+# Given a string print all palindromes present in that string.
+#
+# Approach:
+#
+# Complexity:
+#
+
+def main():
+  a = []
+  n = input('Enter number of elements: ')
+  for i in xrange (n):
+      x = input('Enter value of a[%d]: ' % i)
+      a.append(x)
+
+
+if __name__ == '__main__':
+  main()
