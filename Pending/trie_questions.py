936ea4e2eeeec2c3829a554f54a235029f3583f5
Hansraj Das <raj.das.136@gmail.com>
2018-09-30 21:30:49 +0530

    trie, more urls added

--- Pending/trie_questions.py
+++ Pending/trie_questions.py
@@ -6,6 +6,8 @@
 # 1. Insert and search in trie: https://www.geeksforgeeks.org/trie-insert-and-search/
 # 2. Delete from a trie: https://www.geeksforgeeks.org/trie-delete/
 # 3. Pattern matching using trie suffixes: https://www.geeksforgeeks.org/pattern-searching-using-trie-suffixes/
+# 4. Sort array of strings: https://www.geeksforgeeks.org/sorting-array-strings-words-using-trie/
+# 5. Auto complete feature: https://www.geeksforgeeks.org/auto-complete-feature-using-trie/
 #
 # Approach:
 #
