e6bccaf9c620bba1684c8dd5cf293d3e1e92b008
Hansraj Das <raj.das.136@gmail.com>
2018-10-01 16:55:27 +0530

    Sorted order of words, using trie

--- Pending/trie_questions.py
+++ Pending/trie_questions.py
@@ -3,7 +3,6 @@
 # Date: 2018-
 #
 # Description:
-# - Sort array of strings: https://www.geeksforgeeks.org/sorting-array-strings-words-using-trie/
 # - Pattern matching using trie suffixes: https://www.geeksforgeeks.org/pattern-searching-using-trie-suffixes/
 # - Longest prefix matching: https://www.geeksforgeeks.org/longest-prefix-matching-a-trie-based-solution-in-java/
 #
