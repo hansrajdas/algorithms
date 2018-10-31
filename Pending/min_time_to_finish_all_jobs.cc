48c7692b2c4529e89d909dc224261c8f9c4aae2c
Hansraj Das <hansrajd@google.com>
2018-10-31 11:55:51 +0530

    min time to finish all jobs

---
+++ Pending/min_time_to_finish_all_jobs.cc
@@ -0,0 +1,29 @@
+/*
+ * Date: 2018-
+ *
+ * Description:
+ * https://www.geeksforgeeks.org/find-minimum-time-to-finish-all-jobs-with-given-constraints/
+ *
+ * Approach:
+ *
+ * Complexity:
+ *
+ */
+
+#include "iostream"
+
+using namespace std;
+
+int main() {
+  int i = 0;
+  int n = 0;
+  // int *a = NULL; Use vector here
+  cout << "Enter number of elements: ";
+  cin >> n;
+  a = (int *)malloc(sizeof(int) * n);
+  for (i = 0; i < n; i++) {
+    cout << "Enter element[" << i << "]: ";
+    cin >> a[i];
+  }
+  return 0;
+}
