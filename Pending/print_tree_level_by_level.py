a4340e14098cbc2d74c778b8f4eca21c6faac28a
Hansraj Das <raj.das.136@gmail.com>
2018-11-12 22:01:02 +0530

    print tree elements level by level

---
+++ Pending/print_tree_level_by_level.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Date: 2018-
+#
+# Description:
+# Print tree elements level by level in separate lines.
+#
+# Approach:
+# https://www.geeksforgeeks.org/print-level-order-traversal-line-line/
+#
+# Complexity:
+# O(N)
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
