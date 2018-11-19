84c106b91efc38b0dc136855b5691011b015400c
Hansraj Das <raj.das.136@gmail.com>
2018-11-19 20:39:19 +0530

    moved to algo/

---
+++ Pending/tree_inorder_traversal_iterative.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Date: 2018-
+#
+# Description:
+# Recursive programs may cause stack overflow if recursion depth reaches a
+# certain limit. Write an iterative program to do inorder traversal of tree.
+#
+# Approach:
+#
+# Complexity:
+# O(N)
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
