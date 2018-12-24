d9fa55f4ab6688e071a072b821c3ea58606c045d
Hansraj Das <raj.das.136@gmail.com>
2018-12-25 00:36:50 +0530

    programs rarraanged

--- Level-2/pairs_having_given_sum.c
+++ Level-2/pairs_having_given_sum.c
@@ -2,7 +2,7 @@
  * Date: 2018-09-23
  *
  * Description:
- * Find pairs in array having givenn sum.
+ * Find pairs in array having given sum.
  *
  * Approach:
  * Use a hashmap to keep track of what all numbers are already traversed.
@@ -11,32 +11,6 @@
  * O(n) time and space
  */
 
-#include "stdio.h"
-#include "stdlib.h"
-
-void printArray(int arr[], int n, char *msg) {
-  int i = 0;
-  printf("*********** %s *****************\n", msg);
-  for (i = 0; i < n; i++)
-    printf("%d ", arr[i]);
-  printf("\n\n");
-}
-
-int main() {
-  int i = 0;
-  int n = 0;
-  int *a = NULL;
-  printf("Enter number of elements: ");
-  scanf("%d", &n);
-  a = (int *)malloc(sizeof(int) * n);
-  for (i = 0; i < n; i++)
-  {
-    printf("Enter element [%d]: ", i);
-    scanf("%d",&a[i]);
-  }
-  printArray(a, n, "Inserted Array");
-  return 0;
-}
 #include "stdio.h"
 #include "stdlib.h"
 
