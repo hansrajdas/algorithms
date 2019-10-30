42564926b18feae3a6cfe519ae684c6df6e70ccb
nishaagrawal16 <51795688+nishaagrawal16@users.noreply.github.com>
2019-10-31 00:34:20 +0530

    string.h header file is not required (#4)
    Deleted inclusion of string.h header file as it is not required

--- Level-1/second_largest_in_array.c
+++ Level-1/second_largest_in_array.c
@@ -7,12 +7,11 @@
  */
 
 #include "stdio.h"
-#include "string.h"
 
 int main()
 {
   int i = 0;
-  int arr[10] = {40,5,6,1,2,3,22,33,10,2};
+  int arr[10] = {40, 5, 6, 1, 2, 3, 22, 33, 10, 2};
   int n = 10;
   int first = 0;
   int second = 0;
