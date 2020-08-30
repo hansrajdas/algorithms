344bd0633fd2b61e4944e2b17513a657c58653d2
Hansraj Das <raj.das.136@gmail.com>
2020-08-31 00:29:16 +0530

    Add selection sort descending order

--- Level-2/selection_sort.py
+++ Level-2/selection_sort.py
@@ -7,8 +7,7 @@
 #
 # Approach:
 # Finds index of minimum(when sorting in ascending) element from the remaining
-# array and swap it with element at the end of sorted(starting portion of
-# array) sub-array.
+# array and swap it with element at the end of sorted sub-array.
 # While sorting in ascending order minimum element reaches at first place after
 # first iteration of outer loop.
 #
@@ -16,24 +15,30 @@
 # O(n^2)
 
 
-def main():
-    a = []
-    n = input('Enter number of elements: ')
-    for i in range(n):
-        x = input('Enter value of a[%d]: ' % i)
-        a.append(x)
-
-    for i in range(n):
+def selection_sort_ascending(A):
+    for i in range(len(A)):
         min_idx = i
-        for j in range(i + 1, n):
-            if a[i] > a[j]:
+        for j in range(i + 1, len(A)):
+            if A[min_idx] > A[j]:
                 min_idx = j
-        a[i], a[min_idx] = a[min_idx], a[i]  # Swap min with sorted sub-array.
+        A[i], A[min_idx] = A[min_idx], A[i]  # Swap min with sorted sub-array.
 
-    # Sorted array.
-    for i in range(n):
-        print a[i]
+def selection_sort_descending(A):
+    for i in range(len(A)):
+        max_idx = i
+        for j in range(i + 1, len(A)):
+            if A[max_idx] < A[j]:
+                max_idx = j
+        A[max_idx], A[i] = A[i], A[max_idx]
 
+def sort(A):
+    B = A[:]
+    selection_sort_ascending(A)
+    selection_sort_descending(B)
+    return (A, B)
 
-if __name__ == '__main__':
-    main()
+assert sort([3, 4, 5, 2, 1]) == ([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])
+assert sort([3, 4, 5, 2, 1, 6]) == ([1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1])
+assert sort([]) == ([], [])
+assert sort([1]) == ([1], [1])
+assert sort([2, 1]) == ([1, 2], [2, 1])
