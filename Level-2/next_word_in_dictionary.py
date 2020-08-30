4cd8efb7876c78939ca5dfcacdb296583639300d
Hansraj Das <raj.das.136@gmail.com>
2020-08-30 20:50:20 +0530

    Update get next dictionary word - python3 compatible and file header

--- Level-2/next_word_in_dictionary.py
+++ Level-2/next_word_in_dictionary.py
@@ -1,32 +1,37 @@
+# Date: 2018-09-06
+#
+# Description:
+# Given a string, find the next dictionary word using the characters in given
+# string. If given string is the last dictionary word, return 'no answer'
+# For example:
+# 'abc' => 'acb'
+# 'cba' => 'no answer'
+# 'z' => 'no answer'
+#
+# Approach:
+# Scan string from last till we find character which is less than adjacent
+# right char. Swap those 2 chars, this will give the next dictionary word.
+#
+# Complexity:
+# Time - O(n), n is the length of string
+
 def print_next_string(string):
-  length = len(string)
-  ctr = length
-
-  if length == 1:
+  if len(string) == 1:
     if string == 'z' or string == 'Z':
-      print 'no answer'
-    else:
-      print chr(ord(string) + 1)
-    return None
+      return 'no answer'
+    return chr(ord(string) + 1)
 
+  ctr = len(string)
   lis = list(string)
   while ctr > 1:
     if lis[ctr - 1] > lis[ctr - 2]:
       lis[ctr - 1], lis[ctr - 2] = lis[ctr - 2], lis[ctr - 1]
-      print ''.join(lis)
-      return None
+      return ''.join(lis)
     ctr -= 1
-  print 'no answer'
-  return None
+  return 'no answer'
 
-
-def main():
-  n = input('Enter n: ')
-
-  while n:
-    string = raw_input('Enter string: ')
-    print_next_string(string)
-    n -= 1
-
-if __name__  == "__main__":
-    main()
+assert print_next_string('z') == 'no answer'
+assert print_next_string('zzzz') == 'no answer'
+assert print_next_string('cba') == 'no answer'
+assert print_next_string('abc') == 'acb'
+assert print_next_string('a') == 'b'
