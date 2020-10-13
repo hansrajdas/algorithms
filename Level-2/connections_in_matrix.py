ae458233883b9d0daa8afba318e9b1e4b7ea65b7
Hansraj Das <raj.das.136@gmail.com>
2020-10-14 00:19:00 +0530

    Simplify logic to find connections in a matrix

--- Level-2/connections_in_matrix.py
+++ Level-2/connections_in_matrix.py
@@ -19,20 +19,24 @@
 # Complexity:
 # O(m*n)
 
-def get_connections(m, n, matrix):
-  count = 0
-  for row in range(0, m):
-    for col in range(0, n):
-      if matrix[row][col]:
-        if col < n - 1 and matrix[row][col + 1]:
-          count += 1
-        if row < m - 1 and matrix[row + 1][col]:
-          count += 1
-        if row < m - 1 and col < n - 1 and matrix[row + 1][col + 1]:
-          count += 1
-        if row < m - 1 and col > 0 and matrix[row + 1][col - 1]:
-          count += 1
-  return count
+def is_valid(r, c, rows, cols):
+    return r >= 0 and c >= 0 and r < rows and c < cols
+
+def get_connections(rows, cols, matrix):
+    connections = 0
+    for r in range(rows):
+        for c in range(cols):
+            if not matrix[r][c]:
+                continue
+            if is_valid(r + 1, c, rows, cols) and matrix[r + 1][c]:  # Lower
+                connections += 1
+            if is_valid(r, c + 1, rows, cols) and matrix[r][c + 1]:  # Right
+                connections += 1
+            if is_valid(r + 1, c + 1, rows, cols) and matrix[r + 1][c + 1]:  # Right lower
+                connections += 1
+            if is_valid(r - 1, c + 1, rows, cols) and matrix[r - 1][c + 1]:  # Right upper
+                connections += 1
+    return connections
 
 
 def main():
@@ -41,26 +45,26 @@ def main():
     [0, 1, 1, 1],
     [1, 0, 0, 1],
   ]
-  print 'Connections: %d' % get_connections(3, 4, matrix)  # 8
+  assert get_connections(3, 4, matrix) == 8  # 8
 
   matrix = [[1, 1, 1]]
-  print 'Connections: %d' % get_connections(1, 3, matrix)  # 2
+  assert get_connections(1, 3, matrix) == 2  # 2
 
   matrix = [
     [1],
     [0],
     [1],
   ]
-  print 'Connections: %d' % get_connections(3, 1, matrix)  # 0
+  assert get_connections(3, 1, matrix) == 0  # 0
 
   matrix = [[1]]
-  print 'Connections: %d' % get_connections(1, 1, matrix)  # 0
+  assert get_connections(1, 1, matrix) == 0  # 0
 
   matrix = [
     [1, 1],
     [0, 1],
   ]
-  print 'Connections: %d' % get_connections(2, 2, matrix)  # 3
+  assert get_connections(2, 2, matrix) == 3  # 3
 
 
 if __name__ == '__main__':
