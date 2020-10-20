a28ab6ee798e0ff233cef802e2a17fe442e11f27
proquicklearner <54366984+proquicklearner@users.noreply.github.com>
2020-10-21 01:13:03 +0530

    Added documentation

--- Level-3/infixtopostfix.c
+++ Level-3/infixtopostfix.c
@@ -1,51 +1,116 @@
+/*
+ * 
+ * Description:
+ * Convert Infix expression into postfix.
+ *
+ * Approach:
+ * INFIX -> Input String.
+ * S -> Stack of characters.
+ * R_POLISH -> output String.
+ * next -> iterator for INFIX
+ * 
+ * 1. PUSH "(" into the S and concat ")" into INFIX
+ * 2. Until precedence of next is less then top of S POP elements from the stack
+ *    and concat in R_POLISH.
+ * 3. If next and top of the S are not same then PUSH next into S.
+ * 
+ * 
+ * Complexity:
+ * Time - O(n^2), space: O(n)
+ * 
+ * Precedence Table:
+*      |    Symbol    | Input precedence  |  Stack precedence |  Rank  | 
+*      |     +,-      |         1         |         2         |   -1   |
+*      |     *,/      |         3         |         4         |   -1   |
+*      |      ^       |         6         |         5         |   -1   |
+*      |  Alphabets   |         7         |         8         |    1   |
+*      |      (       |         9         |         0         |    -   |
+*      |      )       |         0         |         -         |    -   |
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-typedef struct NODE
+
+
+// precedence of alphabets in input String and stack
+#define INPUTALPHA 7
+#define STACKALPHA 8
+
+#define RANKALPHA 1
+typedef struct PrecedenceTable
 {
     char symbol;
     int precedence;
     int Rank;
-} NODE;
+} PrecedenceTable;
 
-NODE INFIXOperators[] = {{'+', 1, -1}, {'-', 1, -1}, {'*', 3, -1}, {'/', 3, -1}, {'^', 5, -1}, {'(', 9}, {')', 0}},
-     STACKOperators[] = {{'+', 2, -1}, {'-', 2, -1}, {'*', 4, -1}, {'/', 4, -1}, {'^', 6, -1}, {'(', 0}, {')', 0}};
+// IF operators are in input string then we are using this TABLE
+PrecedenceTable INFIXOperators[] = {{'+' , 1, -1},
+                         {'-' , 1, -1},
+                         {'*' , 3, -1},
+                         {'/' , 3, -1}, 
+                         {'^' , 5, -1}, 
+                         {'(' , 9},    //  Prenthesis doesn't have Rank 
+                         {')' , 0}};
+
+// IF operators are in STACK then we are using this TABLE
+PrecedenceTable STACKOperators[] = {{'+' , 2, -1}, 
+                         {'-' , 2, -1}, 
+                         {'*' , 4, -1}, 
+                         {'/' , 4, -1}, 
+                         {'^' , 6, -1}, 
+                         {'(' , 0},      //  Prenthesis doesn't have Rank
+                         {')' , 0}};
 
 int findrank(char a)
 {
-    int rank = 1;
     for (int i = 0; i < 6; i++)
     {
-        //printf("%c\t%c\n", a, INFIXOperators[i].symbol);
         if (a == INFIXOperators[i].symbol)
+            return INFIXOperators[i].Rank;
+    }
+
+    return RANKALPHA;
+}
+
+int isHigher(char input, char stack)
+{
+    int inputPrec=0, stackPrec=0;
+
+    if(isalpha(input))
+        inputPrec = INPUTALPHA;
+    
+    if(isalpha(stack))
+        stackPrec = STACKALPHA;
+    
+
+    if(inputPrec != INPUTALPHA)
+    {
+        for (int i = 0; i < 7; i++)
         {
-            rank = INFIXOperators[i].Rank;
-            break;
+            if (input == INFIXOperators[i].symbol)
+                inputPrec = INFIXOperators[i].precedence;
         }
     }
 
-    return rank;
-}
-int ishigherprec(char a, char b)
-{
-    int aprec = 7, bprac = 8;
-    for (int i = 0; i < 7; i++)
+    if(stackPrec != STACKALPHA)
     {
-        if (a == INFIXOperators[i].symbol)
-            aprec = INFIXOperators[i].precedence;
-        if (b == STACKOperators[i].symbol)
-            bprac = STACKOperators[i].precedence;
+        for (int i = 0; i < 7; i++)
+        {
+            if (stack == STACKOperators[i].symbol)
+                stackPrec = STACKOperators[i].precedence;
+        }
     }
 
-    //printf("%d\t%d\n", aprec, bprac);
-
-    if (aprec > bprac)
+    if (inputPrec < stackPrec)
     {
         return 1;
     }
-    else if (aprec == bprac)
+    else if (inputPrec == stackPrec)
         return 0;
     else
         return -1;
@@ -71,71 +136,58 @@ char POP(STACK *s)
     return temp;
 }
 
-void display(STACK *s)
-{
-    for (int i = 0; i < s->top; i++)
-    {
-        printf("%c", s->data[i]);
-    }
-    printf("\n");
-}
-
 int main()
 {
-    char *INFIX, *R_POLISH, i, next;
-    int len = 0, Plen = 0, RANK = 0, a = 0;
+    char *INFIX, *R_POLISH, next;
+    int  length = 0, RANK = 0, i = 0;
     STACK S;
 
     INFIX = (char *)malloc(sizeof(char) * 100);
     R_POLISH = (char *)malloc(sizeof(char) * 100);
-    R_POLISH[0] = '\0';
+
+    R_POLISH[length] = '\0';
+
     S.top = 0;
     S.data = NULL;
     PUSH(&S, '(');
 
-    scanf("%c", &i);
-    while (i != '\n')
-    {
-        INFIX[len] = i;
-        len++;
-        scanf("%c", &i);
-    }
+    scanf("%s", INFIX);
+    strcat(INFIX,")");
 
-    INFIX[len++] = ')';
-    INFIX[len] = '\0';
-
-    //puts(INFIX);
-
-    next = INFIX[a++];
+    i = 0;
+    length = 0;
+    next = INFIX[i++];
 
     while (next != '\0')
     {
         if (S.top < 1)
         {
-            printf("top is little");
+            printf("INVALID INFIX EXPRESSION");
             exit(0);
         }
-        while (ishigherprec(next, S.data[S.top - 1]) == -1)
+
+        while (isHigher(next, S.data[S.top - 1]) == 1)
         {
             char temp = POP(&S);
-            R_POLISH[Plen++] = temp;
-            //display(&S);
+            R_POLISH[length++] = temp;
+
             RANK += findrank(temp);
             if (RANK < 1)
             {
-                printf("rank is small");
+                printf("INVALID INFIX EXPRESSION");
                 exit(0);
             }
         }
-        if (ishigherprec(next, S.data[S.top - 1]) != 0)
+
+        if (isHigher(next, S.data[S.top - 1]) != 0)
         {
             PUSH(&S, next);
         }
         else
             POP(&S);
-        next = INFIX[a++];
+        next = INFIX[i++];
     }
 
-    R_POLISH[Plen] = '\0';
+    R_POLISH[length] = '\0';
     puts(R_POLISH);
 }
