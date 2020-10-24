11f2307516cca88498830974550c166ea95684aa
Hansraj Das <raj.das.136@gmail.com>
2020-10-24 18:04:03 +0530

    Add more test cases, python 3 compatible, add note

--- Level-3/cycle_in_graph.py
+++ Level-3/cycle_in_graph.py
@@ -40,7 +40,7 @@ class Graph(object):
     visited[current_vertex] = True
 
     for x in self.graph[current_vertex]:
-      if visited.has_key(x):
+      if x in visited:
         if not visited[x]:
           return self.cycle_wrt_vertex(x, visited)
         else:
@@ -49,7 +49,10 @@ class Graph(object):
   def check_cycle(self):
     """Checks if cycle is present in a graph."""
     for k in self.graph:
-      visited = {k: False for k in self.graph}
+      # This can't be done using set as we have to check for back edge and node
+      # which has an outgoing edge can only have back edge so to keep track of
+      # nodes having outgoing edges, we has to use dict with True/False values
+      visited = {n: False for n in self.graph}
       has_cycle = self.cycle_wrt_vertex(k, visited)
       if has_cycle:
         print('Graph has cycle!')
@@ -57,11 +60,24 @@ class Graph(object):
         break
 
 
+# Case: 1
 g = Graph()
 g.add_edge(10, 11)
 g.add_edge(10, 12)
 g.add_edge(11, 13)
 g.add_edge(13, 14)
-g.add_edge(15, 15)
+g.add_edge(15, 15)  # Create cycle
+
+g.check_cycle()
+
+# Case: 2
+g = Graph()
+
+g.add_edge('a', 'd')
+g.add_edge('f', 'b')
+g.add_edge('b', 'd')
+g.add_edge('f', 'a')
+g.add_edge('d', 'c')
+g.add_edge('d', 'a')  # Create cycle
 
 g.check_cycle()
