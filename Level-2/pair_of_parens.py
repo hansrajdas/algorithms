d17da0f2a1b92edaf1d80d8bad4cdae5666ac1f9
Hansraj Das <raj.das.136@gmail.com>
2020-10-29 00:48:44 +0530

    Rewrite pair of parens problem

--- Level-2/pair_of_parens.py
+++ Level-2/pair_of_parens.py
@@ -10,40 +10,31 @@
 # Output: ((())), (()()), (())(), ()(()), ()()()
 #
 # Approach:
-# List of possible valid parens for any number, n can be generated with list of
-# parens with n = n - 1. So we can follow recursive approach to build this list
-# with any n. Suppose we have list of parens for n = n - 1 called SOLUTION so
-# for n = n we can generate by adding parens to the left, right and enclosing
-# to each item of SOLUTION: () + SOLUTION, (SOLUTION) + SOLUTION + ().
-# One exception in this for simple parens - ()()()...() we just have to add ()
-# to the left or right otherwise it will give us repeated result as:
-# ()...() + '()' will be same as '()' + ()...()
-# For example:
-# n = 1 => ()
-# n = 2 => ()(), (())
-# n = 3 => Use above results:
-# ()() => Adding enclosing and left => ()()(), (()())
-# (()) => Adding enclosing, left and right => ((())), ()(()), (())()
+# On each recursive call, we have the index for a particular in the string. We
+# need to select either a left or right paren.
+# - We will use left paren, as long as we haven't used up all the left
+#   parenthesis, we can always insert a left paren.
+# - We will use right paren as long as it won't lead to a syntax error, that is
+#   we have more right parens than left.
+#
+# For more explaination check problem 8.9 of cracking the coding interview(6th
+# edition)
 #
 # Complexity:
-# Around O(n * n!)
+# ...
 
-def generatePairOfParens(n):
-  if n == 1:
-    return ['()']
-  parens = generatePairOfParens(n - 1)
-  newParensPairs = []
-  for idx in range(len(parens)):
-    newParensPairs.append('()' + parens[idx])
-    newParensPairs.append('(' + parens[idx] + ')')
-
-    # Symmetric parens(list of open/close parens '()()...()') is stored at 0th
-    # index we should not add '()' at the end of this pair otherwise we would
-    # get repeated entries i.e '()' + ()() will be same as ()() + '()' so
-    # skipping first index while suffixing '()', only prefixed in this case.
-    if idx:
-      newParensPairs.append(parens[idx] + '()')
-  return newParensPairs
+def generatePairOfParens(left_rem, right_rem, holder, idx, parens):
+    if left_rem < 0 or right_rem < left_rem:
+        return
+    if not (left_rem or right_rem):
+        parens.append(''.join(holder))
+    else:
+        if left_rem > 0:
+            holder[idx] = '('
+            generatePairOfParens(left_rem - 1, right_rem, holder, idx + 1, parens)
+        if right_rem > left_rem:
+            holder[idx] = ')'
+            generatePairOfParens(left_rem, right_rem - 1, holder, idx + 1, parens)
 
 def main():
   n = input("Enter number: ")
@@ -52,41 +43,49 @@ def main():
   except ValueError:
     print('Invalid input')
     return
-  parens = generatePairOfParens(int(n))
+  parens = []
+  holder = [''] * n * 2
+  generatePairOfParens(n,  # Left braces remaining
+                       n,  # Right braces remaining  
+                       holder,  # Var to store one valid paren expression
+                       0,  # Index for holder
+                       parens)  # Store response
   for idx in range(len(parens)):
     print('{idx}: {parens}'.format(idx=idx + 1, parens=parens[idx]))
 
 if __name__ == '__main__':
   main()
 
+
 # Output:
-#
-# Enter input string: 1
+# --------
+# (env) hansrajdas@Hansrajs-MacBook-Air ~/code/algorithms$ python Level-2/pair_of_parens.py
+# Enter number: 1
 # 1: ()
-#
-# Enter input string: 2
-# 1: ()()
-# 2: (())
-#
-# Enter input string: 3
-# 1: ()()()
+# (env) hansrajdas@Hansrajs-MacBook-Air ~/code/algorithms$ python Level-2/pair_of_parens.py
+# Enter number: 2
+# 1: (())
+# 2: ()()
+# (env) hansrajdas@Hansrajs-MacBook-Air ~/code/algorithms$ python Level-2/pair_of_parens.py
+# Enter number: 3
+# 1: ((()))
 # 2: (()())
-# 3: ()(())
-# 4: ((()))
-# 5: (())()
-#
-# Enter input string: 4
-# 1: ()()()()
-# 2: (()()())
-# 3: ()(()())
-# 4: ((()()))
-# 5: (()())()
-# 6: ()()(())
-# 7: (()(()))
-# 8: ()(())()
-# 9: ()((()))
-# 10: (((())))
-# 11: ((()))()
+# 3: (())()
+# 4: ()(())
+# 5: ()()()
+# (env) hansrajdas@Hansrajs-MacBook-Air ~/code/algorithms$ python Level-2/pair_of_parens.py
+# Enter number: 4
+# 1: (((())))
+# 2: ((()()))
+# 3: ((())())
+# 4: ((()))()
+# 5: (()(()))
+# 6: (()()())
+# 7: (()())()
+# 8: (())(())
+# 9: (())()()
+# 10: ()((()))
+# 11: ()(()())
 # 12: ()(())()
-# 13: ((())())
-# 14: (())()()
+# 13: ()()(())
+# 14: ()()()()
