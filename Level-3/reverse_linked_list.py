8d8fbdde430fc1a35f1033cd90eb611646dd3065
Hansraj Das <raj.das.136@gmail.com>
2021-06-13 17:38:48 +0530

    Add fn to reverse linked list using insert at head

--- Level-3/reverse_linked_list.py
+++ Level-3/reverse_linked_list.py
@@ -13,6 +13,9 @@
 # Reference:
 # https://medium.com/outco/reversing-a-linked-list-easy-as-1-2-3-560fbffe2088
 #
+# Another simple way of reversing linked list is to process each node and keep
+# on inserting at another head, explained here: https://leetcode.com/problems/reverse-nodes-in-k-group/solution/
+#
 # Complexity:
 # O(n)
 
@@ -30,7 +33,7 @@ class LinkedList:
   def traverse(self):
     current = self.head
     while current:
-      print current.data
+      print(current.data, end=' ')
       current = current.next
 
   def insert_at_end(self, data):
@@ -57,9 +60,31 @@ class LinkedList:
       current = nxt
     self.head = previous
 
+  def reverse_list_using_insert_at_head(self, head, k):
+      """
+      This is simple way of reversing linked list - process each node and keep
+      on inserting at another head.
+      Explained here(premium): https://leetcode.com/problems/reverse-nodes-in-k-group/solution/
+      """
+      # Reverse k nodes of the given linked list.
+      # This function assumes that the list contains 
+      # atleast k nodes.
+      new_head = None
+      ptr = head
+      while k:
+          next_node = ptr.next
+          
+          # Insert the node pointed to by "ptr"
+          # at the beginning of the reversed list
+          ptr.next = new_head
+          new_head = ptr
+          ptr = next_node
+          k -= 1
+      return new_head
+
+
 def main():
   linked_list = LinkedList()
-
   linked_list.insert_at_end(1)
   linked_list.insert_at_end(2)
   linked_list.insert_at_end(3)
@@ -68,7 +93,21 @@ def main():
   linked_list.traverse()
 
   linked_list.reverse()
-  print ('\nReversed linked list')
+  print('\nReversed linked list', end=' ')
+  linked_list.traverse()
+
+  print()
+  linked_list = LinkedList()
+  linked_list.insert_at_end(1)
+  linked_list.insert_at_end(2)
+  linked_list.insert_at_end(3)
+  linked_list.insert_at_end(4)
+  linked_list.insert_at_end(5)
+  linked_list.traverse()
+
+  rev_head = linked_list.reverse_list_using_insert_at_head(linked_list.head, 5)
+  print('\nReversing by insert at head', end=' ')
+  linked_list.head = rev_head
   linked_list.traverse()
 
 
