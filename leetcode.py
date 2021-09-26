121cbd483d75034c15cc962b94c0a6ca1bb8316f
Hansraj Das <raj.das.136@gmail.com>
2021-09-27 01:31:57 +0530

    Add leetcode file - helps to debug leetcode proograms

---
+++ leetcode.py
@@ -0,0 +1,22 @@
+class Solution:
+    def restoreIpAddresses(self, s):
+        def solve(i, values, results):
+            # import pdb;pdb.set_trace()
+            print(i, values)
+            if i == len(s) and len(values) == 4:
+                results.append('.'.join(values))
+                return
+            for j in range(i, min(i + 3, len(s))):
+                if 0 <= int(s[i:j+1]) <= 255:
+                    values.append(s[i:j+1])
+                    solve(j+1, values, results)
+                    values.pop()
+
+        print(len(s), s)
+        results = []
+        solve(0, [], results)
+        print(results)
+        return results
+
+s = Solution()
+print(s.restoreIpAddresses("25525511135"))  # 1
