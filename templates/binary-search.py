5fa47134ab2b3198a0efc428aa14bc575391e952
Hansraj Das <hansrajd@vmware.com>
2022-10-02 01:17:17 +0530

    Add templates dir

---
+++ templates/binary-search.py
@@ -0,0 +1,12 @@
+def binary_search(nums, x):
+    low = 0
+    high = len(nums) - 1
+    while low < high:
+        mid = (high + low) // 2
+        if nums[mid] == x:
+            return mid
+        elif nums[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
