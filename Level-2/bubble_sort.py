388e5650d8e2ded810ccdfedcf1615fd5cbbee50
Hansraj Das <raj.das.136@gmail.com>
2023-12-28 22:05:07 +0530

    Update descending bubble sort - same pattern as ascending

--- Level-2/bubble_sort.py
+++ Level-2/bubble_sort.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 
-# Date: 2018-09-08
-#
-# Description:
-# Implement bubble sort.
-#
-# Approach:
-# Compares adjacent elements and pushes to it's required extreme. While
-# sorting in ascending order largest element reaches at last place after first
-# iteration of outer loop. Vice versa for descending.
-#
-# Complexity:
-# O(n^2)
+"""
+Date: 2018-09-08
 
+Description:
+Implement bubble sort.
+
+Approach:
+Compares adjacent elements and pushes to it's extreme end. While
+sorting in ascending order largest element reaches at last place after first
+iteration of outer loop. For descending smallest reaches to the last place.
+
+Complexity:
+O(n^2)
+"""
 
 def bubble_sort_ascending(A):
     for i in range(len(A)):
@@ -26,17 +27,14 @@ def bubble_sort_ascending(A):
             break
 
 def bubble_sort_descending(A):
-    i = 0
-    while i < len(A):
-        j = 1
-        has_swap = False
-        while j < len(A) - i:
-            if A[j - 1] < A[j]:
-                has_swap = True
-                A[j - 1], A[j] = A[j], A[j - 1]
-            j += 1
-        i += 1
-        if not has_swap:
+    for i in range(len(A)):
+        swap_flag = False;
+        for j in range(0, len(A) - 1 - i, 1):
+            if (A[j] < A[j + 1]):
+                A[j], A[j + 1] = A[j + 1], A[j]
+                swap_flag = True;
+
+        if not swap_flag:
             break
 
 def sort(A):
@@ -46,6 +44,7 @@ def sort(A):
     return (A, B)
 
 
+assert sort([5, 4, 3, 2, 1, 10, 28, 7, 6]) == ([1, 2, 3, 4, 5, 6, 7, 10, 28], [28, 10, 7, 6, 5, 4, 3, 2, 1])
 assert sort([3, 4, 5, 2, 1]) == ([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])
 assert sort([3, 4, 5, 2, 1, 6]) == ([1, 2, 3, 4, 5, 6], [6, 5, 4, 3, 2, 1])
 assert sort([]) == ([], [])
